3f7db8452a913f26ab91ce682ea22eb0de8d1fec
Vigneshd705 <vigneshd705@gmail.com>
2024-11-30 19:00:13 +0530

    secure

--- myapp/views.py
+++ myapp/views.py
@@ -5,13 +5,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-
-genai_api_key = os.getenv("GENAI_API_KEY")
-
-if not genai_api_key:
-    raise ValueError("GENAI_API_KEY environment variable is not set")
-
-genai.configure(api_key=genai_api_key)
+genai.configure(api_key="GENAI_API_KEY")
 
 
 # Create the model
